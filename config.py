f23637cc4f13ff8f098eb2983b97864cd99547b5
Emi1699 <emanuelbuliga16@gmail.com>
2023-03-05 15:47:26 +0200

    gitingore update

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # replace **insert your API key here** with your actual API keys
-OPENAI_API_KEY = "**insert your API key here**" # TODO: Add your API key in the string
-ELEVENLABS_API_KEY = "**insert your API key here**" # TODO: Add your API key in the string
+OPENAI_API_KEY = "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" # TODO: Add your API key in the string
+ELEVENLABS_API_KEY = "37bf1bb5d5cdbbcfe79ac05775bed5a2" # TODO: Add your API key in the string
 VOICE_ID = "**insert your API key here**"
